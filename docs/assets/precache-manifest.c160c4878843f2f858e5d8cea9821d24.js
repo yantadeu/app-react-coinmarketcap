0fdc75d0952b52f09e3615d3d5db13d0dc9096ce
Yan Tadeu <yan.tadeu1@gmail.com>
2018-11-01 09:59:25 -0300

    Atualização package.json para start em produção

---
+++ docs/assets/precache-manifest.c160c4878843f2f858e5d8cea9821d24.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = [
+  {
+    "url": "/assets/vendors.b9d7294840e6b5f30ed9.js.gz"
+  },
+  {
+    "url": "/assets/vendors.b9d7294840e6b5f30ed9.js"
+  },
+  {
+    "url": "/assets/styles.b9d7294840e6b5f30ed9.js"
+  },
+  {
+    "revision": "99b7a4f73bde262ba83a1d3358de6805",
+    "url": "/assets/modernizr-bundle.js"
+  },
+  {
+    "url": "/assets/app.b9d7294840e6b5f30ed9.js.gz"
+  },
+  {
+    "url": "/assets/app.b9d7294840e6b5f30ed9.js"
+  },
+  {
+    "url": "/assets/4.b9d7294840e6b5f30ed9.js"
+  },
+  {
+    "url": "/assets/3.b9d7294840e6b5f30ed9.js"
+  },
+  {
+    "revision": "2ceda11b12a2fa88ec0da6c75be64659",
+    "url": "/assets/../index.html"
+  }
+];
